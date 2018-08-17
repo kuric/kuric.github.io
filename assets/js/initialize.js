3f87f7f6bf40810d18a9738cd2b6a0f70dd50bb8
Oleksandr Andrieiev <kuricyak@gmail.com>
2018-08-17 14:31:48 +0300

    added certificates

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -57,7 +57,22 @@ function initializeScripts(){
     startingTop: '30%', // Starting top style attribute
     endingTop: '35%',
   })
-
+  $('.certifications .modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .8, // Opacity of modal background
+    inDuration: 200, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '0%', // Starting top style attribute
+    endingTop: '0%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      // alert("Ready");
+      modal_open = true
+      $('.carousel.carousel-slider').carousel({fullWidth: true});
+    },
+    complete: function(e) {
+      // debugger
+    } // Callback for Modal close
+  });
   $('.project_list .modal').modal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .8, // Opacity of modal background
